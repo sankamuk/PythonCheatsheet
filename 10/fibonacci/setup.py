7d79d5afcc3ff01863af77656f982927a44457b2
Sankar Mukherjee <sanmuk21@gmail.com>
2022-02-25 13:29:13 +0530

    Complete Chapter 10

---
+++ 10/fibonacci/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Fibonacci",
+    description="Fibonacci Number Generator",
+    version="0.1-SNAPSHOT",
+    author="Sankar Mukherjee",
+    author_email="my_email@something.com",
+    license="Apache License 2.0",
+    py_modules=["fibonacci"],
+    keywords="fibonacci"
+)
